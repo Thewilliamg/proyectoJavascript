9aef8169038932a4a9cfdaaef90807dd208b045f
Thewilliamg <156461507+Thewilliamg@users.noreply.github.com>
2024-05-14 13:24:43 -0500

    feat: :construction: Añadido json de album track y fetch aplicado inicial

---
+++ components/fetchAlbum.js
@@ -0,0 +1,34 @@
+export const importationAlbum = async() => {
+    const url = 'https://spotify23.p.rapidapi.com/albums/?ids=3IBcauSj5M2A6lTeffJzdv';
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '1095f8b652msh95f265095f02f92p1dd4c1jsncde0e9ba16cf',
+            'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+        }
+    };
+    try {
+    const response = await fetch(url, options);
+    const result = await response.json();
+    console.log(result);
+    } catch (error) {
+        console.error(error);
+    }
+}
+export const importationAlbumTracks = async() => {
+    const url = 'https://spotify23.p.rapidapi.com/album_tracks/?id=3IBcauSj5M2A6lTeffJzdv&offset=0&limit=300';
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '1095f8b652msh95f265095f02f92p1dd4c1jsncde0e9ba16cf',
+            'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+        }
+    };
+    try {
+        const response = await fetch(url, options);
+        const result = await response.text();
+        console.log(result);
+    } catch (error) {
+        console.error(error);
+    }
+}
---
+++ components/fetchSearch.js
@@ -0,0 +1,19 @@
+export const importationSearch = async() => {
+    const url = 'https://spotify23.p.rapidapi.com/search/?q=%3CREQUIRED%3E&type=multi&offset=0&limit=20&numberOfTopResults=5';
+    const urljson = '../db/album.json'
+
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '1095f8b652msh95f265095f02f92p1dd4c1jsncde0e9ba16cf',
+            'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+        }
+    };
+    try {
+        const response = await fetch(urljson, options);
+        const result = await response.json();
+        console.log(result);
+    } catch (error) {
+        console.error(error);
+    }
+}
